776a2cfd11a9b56c82bf49e80fe0acbde64a5708
Mehdi Kiani <mehdi.kiani@gmail.com>
2016-04-03 18:49:13 +0430

    changed

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-
-var friends = ["Mehdi", "Sepideh", "Touka"];
-
-app.get("/", function(req, res){
-	res.render("home");
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello world!');
 });
-
-app.get("/friends", function(req, res){
-	res.render("friends", {friends: friends});
-});
-
-app.post("/addfriend", function(req, res){
-	var newFriend = req.body.newfriend;
-	friends.push(newFriend);
-	res.redirect("/friends");
-})
-
-
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
