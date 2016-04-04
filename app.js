8d53cabf3df9ee0cab79961a62813e6c370c904b
Mehdi Kiani <mehdi.kiani@gmail.com>
2016-04-04 18:32:50 +0430

    ADD mongoose

--- app.js
+++ app.js
@@ -1,21 +1,66 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/friends");
 
-var friends = ["Mehdi", "Sepideh", "Touka"];
+var friendSchema = new mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+var Friend = mongoose.model("Friend", friendSchema);
+
+
+
+// Friend.create({
+// 	name: "Mehdi",
+// 	age: 37
+// },function(err, friend){
+// 	if(err){
+// 		console.log("Error!");
+// 	} else {
+// 		console.log("Friend added!");
+// 	}
+// });
+
+// Friend.create({
+// 	name: "Sepideh",
+// 	age: 29
+// },function(err, friend){
+// 	if(err){
+// 		console.log("Error!");
+// 	} else {
+// 		console.log("Friend added!");
+// 	}
+// });
+
+// Friend.find({}, function(err, friends){
+// 	if(err)
+// 	{
+// 		console.log("OH NOOOO!!!!");
+// 	} else {
+// 		console.log("This are my friends....");
+// 		console.log(friends);
+// 	}
+// })
+
+
+
+// var friends = ["Mehdi", "Sepideh", "Touka"];
 
 app.get("/", function(req, res){
 	res.render("home");
 });
 
 app.get("/friends", function(req, res){
-	res.render("friends", {friends: friends});
+	Friend.find({}, function(err, friends){
+		res.render("friends", {friends: friends});
+	});
 });
-
 app.post("/addfriend", function(req, res){
 	var newFriend = req.body.newfriend;
 	friends.push(newFriend);
@@ -23,4 +68,4 @@ app.post("/addfriend", function(req, res){
 })
 
 
-app.listen(process.env.PORT);
+app.listen(env.process.PORT);
